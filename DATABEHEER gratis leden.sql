f716893081d3add39723c58a57e25e35b821c2ed
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2021-10-13 12:10:46 +0200

    Create DATABEHEER gratis leden.sql

---
+++ DATABEHEER gratis leden.sql
@@ -0,0 +1,14 @@
+SELECT p.id, p.create_date::date, p.name naam, 
+		--p.street adres, p.zip postcode, p.city woonplaats, 
+		pfc.name "type", p.free_member_comment omschrijving, /*p.free_membership_class_id,*/ p.active
+FROM res_partner p
+	LEFT OUTER JOIN res_partner_free_class pfc ON p.free_membership_class_id = pfc.id
+WHERE p.free_member AND p.active 
+		--AND pfc.name = 'Pers'
+ORDER BY pfc.name
+
+/*
+SELECT * FROM res_partner p WHERE p.free_member AND free_member_comment LIKE '%1%'
+
+SELECT * FROM res_partner_free_class
+*/
