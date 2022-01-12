e6fb03a8bb5570d0e3726a1dd746351a496399c6
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2022-01-12 17:05:32 +0100

    Update DATABEHEER ruilabonnementen.sql
    aangepast om enkel het laatste jaar te selecteren

--- DATABEHEER ruilabonnementen.sql
+++ DATABEHEER ruilabonnementen.sql
@@ -1,5 +1,6 @@
 SELECT ml.partner, p.name, ml.date_from, ml.date_to, ml.state, ml.member_price, pp.name_template, ml.remarks
 FROM membership_membership_line ml
+	JOIN (SELECT max(ml.id) id FROM membership_membership_line ml WHERE LOWER(ml.remarks) = 'ruilabonnement' GROUP BY ml.partner) SQ1 ON SQ1.id = ml.id
 	INNER JOIN res_partner p ON ml.partner = p.id
 	JOIN product_product pp ON pp.id = ml.membership_id
 WHERE LOWER(ml.remarks) = 'ruilabonnement'
