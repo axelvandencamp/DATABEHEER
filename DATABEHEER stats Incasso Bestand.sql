414235036434ddc09c0dfd7872074bc43939b650
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2022-03-21 15:24:31 +0100

    Create DATABEHEER stats Incasso Bestand.sql

---
+++ DATABEHEER stats Incasso Bestand.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS _AV_myvar;
+CREATE TEMP TABLE _AV_myvar 
+	(incasso_bestanden character varying[]
+	 );
+
+INSERT INTO _AV_myvar VALUES(	
+				ARRAY['2022/05052','2022/05053']
+				);
+SELECT * FROM _AV_myvar;
+--------------------------------------------------------------------------------
+SELECT po.reference, sm.recurrent_sequence_type "<seqTP>", SUM(amount_currency) bedrag, COUNT(pl.id) aantal
+FROM _AV_myvar v, payment_line pl
+	JOIN payment_order po ON pl.order_id = po.id 
+	JOIN sdd_mandate sm ON pl.sdd_mandate_id = sm.id
+WHERE po.reference IN v.incasso_bestanden
+GROUP BY po.reference, sm.recurrent_sequence_type
