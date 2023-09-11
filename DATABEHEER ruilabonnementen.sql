049680ec50e442d55819ba37a2be9237b3375df6
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2023-09-11 10:05:00 +0200

    Update DATABEHEER ruilabonnementen.sql
    controle op "gratis" abonnementen toegevoegd;

--- DATABEHEER ruilabonnementen.sql
+++ DATABEHEER ruilabonnementen.sql
@@ -1,6 +1,10 @@
 SELECT ml.partner, p.name, ml.date_from, ml.date_to, ml.state, ml.member_price, pp.name_template, ml.remarks
 FROM membership_membership_line ml
-	JOIN (SELECT max(ml.id) id FROM membership_membership_line ml WHERE LOWER(ml.remarks) LIKE 'ruilabonnement%' GROUP BY ml.partner) SQ1 ON SQ1.id = ml.id
+	JOIN (SELECT max(ml.id) id 
+		  FROM membership_membership_line ml 
+		  WHERE (LOWER(ml.remarks) LIKE 'ruilabonnement%' OR LOWER(ml.remarks) LIKE 'gratis') 
+		  GROUP BY ml.partner) SQ1 ON SQ1.id = ml.id
 	INNER JOIN res_partner p ON ml.partner = p.id
 	JOIN product_product pp ON pp.id = ml.membership_id
-WHERE LOWER(ml.remarks) LIKE 'ruilabonnement%'
+WHERE (LOWER(ml.remarks) LIKE 'ruilabonnement%' OR LOWER(ml.remarks) LIKE 'gratis')
+	--AND ml.partner = 20353
