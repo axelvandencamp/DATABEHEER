54a5e3821555a14f42c4dce2fe7ad95d6b3b0296
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2023-11-24 15:46:21 +0100

    Update DATABEHEER product info ophalen.sql

--- DATABEHEER product info ophalen.sql
+++ DATABEHEER product info ophalen.sql
@@ -6,7 +6,7 @@ CREATE TABLE marketing._m_temp_prodinfotoevoegen (
 	Lidmaatschapslijn integer,
 	OGM text,
 	bedrag numeric,
-	type_digitaal text)
+	type_digitaal text);
 	
 SELECT * FROM marketing._m_temp_prodinfotoevoegen;	
 ------------------------------------------------------------------	
@@ -19,11 +19,11 @@ INSERT INTO marketing._m_temp_prodinfotoevoegen (
 		JOIN (
 			SELECT MAX(ml.id) id, ml.partner 
 			FROM membership_membership_line ml JOIN product_product pp ON pp.id = ml.membership_id
-			WHERE  (ml.date_to BETWEEN '2022-01-01' and '2022-12-31' OR '2022-12-31' BETWEEN ml.date_from AND ml.date_to) 
+			WHERE  (ml.date_to BETWEEN '2023-01-01' and '2023-12-31' OR '2023-12-31' BETWEEN ml.date_from AND ml.date_to) 
 				AND pp.membership_product
 			GROUP BY ml.partner
 			) sq1
-			ON sq1.partner = p.id)
+			ON sq1.partner = p.id);
 			
 --mededeling toevoegen
 UPDATE marketing._m_temp_prodinfotoevoegen t
