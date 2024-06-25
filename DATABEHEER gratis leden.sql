e1cc2e5419700f343ea92d9f421e2e74bfd3c0d6
kristienv <88887626+kristienv@users.noreply.github.com>
2024-06-25 15:12:05 +0200

    Update DATABEHEER gratis leden.sql

--- DATABEHEER gratis leden.sql
+++ DATABEHEER gratis leden.sql
@@ -1,8 +1,9 @@
-SELECT p.id, p.create_date::date, p.name naam, 
+SELECT p.id, p.create_date::date, p.name naam, mo.name herkomst,
 		--p.street adres, p.zip postcode, p.city woonplaats, 
 		pfc.name "type", p.free_member_comment omschrijving, /*p.free_membership_class_id,*/ p.active
 FROM res_partner p
 	LEFT OUTER JOIN res_partner_free_class pfc ON p.free_membership_class_id = pfc.id
+	LEFT OUTER JOIN res_partner_membership_origin mo ON mo.id = p.membership_origin_id
 WHERE p.free_member AND p.active 
 		--AND pfc.name = 'Pers'
 ORDER BY pfc.name
