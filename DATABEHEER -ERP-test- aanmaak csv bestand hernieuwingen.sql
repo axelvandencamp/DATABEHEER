a828100c9dcc764f6f356cf8311364e5c15cfae9
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2024-10-29 13:41:46 +0100

    Update DATABEHEER -ERP-test- aanmaak csv bestand hernieuwingen.sql

--- DATABEHEER -ERP-test- aanmaak csv bestand hernieuwingen.sql
+++ DATABEHEER -ERP-test- aanmaak csv bestand hernieuwingen.sql
@@ -1,5 +1,8 @@
 SELECT p.id, p.name, p.membership_state
 FROM res_partner p
-WHERE p.membership_state IN ('paid','invoiced','free')
+	--bank/mandaat info
+	LEFT OUTER JOIN (SELECT pb.id pb_id, pb.partner_id pb_partner_id, sm.id sm_id, sm.state sm_state FROM res_partner_bank pb JOIN sdd_mandate sm ON sm.partner_bank_id = pb.id WHERE sm.state = 'valid') sm ON pb_partner_id = p.id
+WHERE p.membership_state IN ('paid','invoiced','free') AND p.active
 	AND p.membership_end <= '2024-12-31' AND NOT(p.membership_stop = '2025-12-31')
 	AND COALESCE(p.membership_cancel,'1900-01-01') = '1900-01-01'
+	AND COALESCE(sm.sm_id,0) > 0
