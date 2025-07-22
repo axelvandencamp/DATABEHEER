78fb4b8e3837a6c79bb927b7016e5c87f67b8974
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2025-07-22 11:36:03 +0200

    Update DATABEHEER contractant vs partner_id.sql

--- DATABEHEER contractant vs partner_id.sql
+++ DATABEHEER contractant vs partner_id.sql
@@ -1,14 +1,19 @@
 -- contracten met gelinkte partner_id
-SELECT p.id, p.name, mpt.name, /*mpt.id,*/ aaa.code, aaa.name AS "kostenplaats/contractant"
+SELECT aaa.code kostenplaats, aaa.name contractant, p.id partner_id, p.name partner, mpt.name contact_type, /*mpt.id,*/ rc.name vzw
 FROM res_partner p
 	JOIN account_analytic_account aaa ON aaa.partner_id = p.id
 	JOIN crm_marketing_partner_type_rel mptr ON mptr.partner_type_id = p.id
 	JOIN res_crm_marketing_partner_type mpt ON mptr.name = mpt.id
+	LEFT OUTER JOIN res_company rc ON rc.id = aaa.company_id
 WHERE p.active AND aaa.active AND COALESCE(p.organisation_type_id,0) = 0
-	AND LOWER(aaa.code) LIKE 'c-cp-%'
-	AND mpt.id = 7		-- 1 = "Bedrijven & verenigingen"; 7 = "Partner Bos voor Iedereen"
+	--AND LOWER(aaa.code) LIKE 'c-cp-%'
+	AND LOWER(aaa.code) = 'c-cp-000495'
+	AND mpt.id IN (1,7)		-- 1 = "Bedrijven & verenigingen"; 7 = "Partner Bos voor Iedereen"
 ORDER BY p.id
 
+SELECT * FROM account_analytic_account aaa WHERE LOWER(aaa.code) LIKE 'c-cp-%' ORDER BY create_date DESC LIMIT 100
+SELECT * FROM account_analytic_account aaa WHERE LOWER(aaa.code) = 'c-cp-000495'
+SELECT * FROM res_company rc WHERE rc.id = 5
 
 -- contracten zonder gelinkte partner_id
 SELECT p.id, p.name, mpt.name, aaa.create_date::date aanmaak_datum, aaa.code, aaa.name AS "kostenplaats/contractant"
