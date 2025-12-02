13cc29abbe02970aa093ccd4423544dda24cde60
kristienv <88887626+kristienv@users.noreply.github.com>
2025-12-02 13:46:04 +0100

    Update DATABEHEER gratis leden.sql

--- DATABEHEER gratis leden.sql
+++ DATABEHEER gratis leden.sql
@@ -4,7 +4,7 @@ SELECT p.id, p.create_date::date, p.name naam, mo.name herkomst,
 FROM res_partner p
 	LEFT OUTER JOIN res_partner_free_class pfc ON p.free_membership_class_id = pfc.id
 	LEFT OUTER JOIN res_partner_membership_origin mo ON mo.id = p.membership_origin_id
-WHERE p.free_member AND p.active AND mo.name IN ('Gratis Lidmaatschap','J.N.M. ouwe sok')
+WHERE p.free_member AND p.active --AND mo.name IN ('Gratis Lidmaatschap','J.N.M. ouwe sok')
 		--AND pfc.name = 'Pers'
 ORDER BY pfc.name
 
@@ -12,4 +12,14 @@ ORDER BY pfc.name
 SELECT * FROM res_partner p WHERE p.free_member AND free_member_comment LIKE '%1%'
 
 SELECT * FROM res_partner_free_class
+
+SELECT p.id, p.create_date::date, p.name naam, mo.name herkomst, p.membership_state,
+		--p.street adres, p.zip postcode, p.city woonplaats, 
+		pfc.name "type", p.free_member_comment omschrijving, /*p.free_membership_class_id,*/ p.active
+FROM res_partner p
+	LEFT OUTER JOIN res_partner_free_class pfc ON p.free_membership_class_id = pfc.id
+	LEFT OUTER JOIN res_partner_membership_origin mo ON mo.id = p.membership_origin_id
+WHERE p.membership_state = 'free'
+ORDER BY pfc.name
+
 */
